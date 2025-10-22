12276330d158d077a597b6d76572d2d6956db837
aramburo reyes francisco eduardo <aramburoeduardo972@gmail.com>
2025-10-21 17:56:07 -0700

    Initial Commit

---
+++ NetworkingStarter/NetworkingStarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingStarterApp.swift
+//  NetworkingStarter
+//
+//  Created by francisco eduardo aramburo reyes on 21/10/25.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingStarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
